e3f380ccc040d0d53633479fee8c822fce5d1dc0
Dillon Min <dillon.minfei@gmail.com>
2024-01-28 13:17:20 +0800

    bmp switch ok
    convert ../examples/logo-8.bmp -define bmp:subtype=RGB565 -depth 16 -crop 240x16 -type truecolor a.bmp
    ./target/debug/host-ili9325 ../;./target/debug/host-ili9325
    
    Signed-off-by: Dillon Min <dillon.minfei@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,9 @@ use std::fs::File;
 use std::io::prelude::*;
 use std::path::Path;
 use colored::Colorize;
+//use md5_rs::Context;
+use crypto::digest::Digest;
+use crypto::md5::Md5;
 
 fn usize_to_u8_array(x: usize) -> [u8; 2] {
   let b1: u8 = ((x >> 8) & 0xff) as u8;
@@ -18,20 +21,28 @@ async fn main() {
     let param = std::env::args().nth(1).expect("no param given");
     let stream = TcpStream::connect("192.168.1.2:1234").await.unwrap();
     let path = Path::new(&param);
-    let display = path.display();
-
-    let mut file = match File::open(&path) {
+    let mut y :u16 = 0;
+//    loop {
+    for i in 0..20 {
+    let s_path = path.join("a-".to_owned() + &i.to_string() + ".bmp");
+    let display = s_path.display();
+    println!("going to send: {}\r", display);
+    let mut file = match File::open(&s_path) {
         Err(err) => panic!("can't open {}: {:?}", display, err),
         Ok(file) => file,
     };
 
-    let mut ctn = String::new();
-    file.read_to_string(&mut ctn).unwrap();
-    //println!("write timeout {}, read timeout {}\r",
-    //         stream.write_timeout().unwrap(),
-    //         stream.read_timeout().unwrap());
+    let mut ctn = Vec::new();
+    file.read_to_end(&mut ctn).unwrap();
+    let mut sh = Md5::new();
+    sh.input(&ctn);
+    let mut digest: [u8; 16] = [0;16];
+    sh.result(&mut digest);
+    //let mut ctx = Context::new();
+    //ctx.read(&ctn.as_bytes());
+    //let digest = ctx.finish();
+    //let hash = digest.iter().map(|x| format!("{:02x}", x)).collect::<String>();
 
-//    loop {
         // 注册可读和可写事件，并等待事件的发生
         let ready = stream.ready(Interest::WRITABLE).await.unwrap();
         //stream.try_write(&usize_to_u8_array(ctn.len())).unwrap();
@@ -41,9 +52,15 @@ async fn main() {
             let mut vec = Vec::new();
             vec.push(file_len[0]);
             vec.push(file_len[1]);
-            vec.extend(ctn.as_bytes());
+            vec.extend(digest);
+            vec.push(0);
+            vec.push(0);
+            vec.push(((y >> 8) & 0xff) as u8);
+            vec.push((y & 0xff) as u8);
+            y = y + 16;
+            vec.extend(ctn);
             //stream.try_write(&file_len).unwrap();
-            println!("file len: {}\r", (file_len[0] as u16) << 8 | file_len[1] as u16);
+            println!("file len: {}, hash {:?}\r", (file_len[0] as u16) << 8 | file_len[1] as u16, digest);
             match stream.try_write(vec.as_slice()) {
                 Ok(n) => {
                     println!("write {} bytes", n);
@@ -65,9 +82,12 @@ async fn main() {
             let mut data = vec![0; 1024];
             match stream.try_read(&mut data) {
                 Ok(n) => {
-                    println!("\r\n\r\n{} read {} bytes, data:\n{}\r", "=======>".red(), n, core::str::from_utf8(&data).unwrap().green());
-                    if n != 1000 {
+                    println!("\r\n\r\n{} read {} bytes, result {}\r", "=======>".red(), n, core::str::from_utf8(&data).unwrap().green());
+                    if i == 19 {
                         return;
+                    }
+                    if n != 1000 {
+                        break;
                     }
                 }
                 Err(ref e) if e.kind() == io::ErrorKind::WouldBlock => {
@@ -81,5 +101,5 @@ async fn main() {
             break;
         }
         }
-  //  }
+    }
 }
